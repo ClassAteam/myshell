71f51a1d26d9481fb6816019fa0c906223dcbabf
ivan <royal_88@mail.ru>
2022-05-13 15:17:07 +0300

    added extra file slot for compilation in benchmark

--- google-bench-compilation.sh
+++ google-bench-compilation.sh
@@ -3,5 +3,5 @@
 name="$1"
 filename=${name::-3}
 
-g++ $1 -I ~/benchmark/include/ -g -O4 -Wall -Wextra -Werror -pedantic --std=c++14 \
+g++ $1 $2 -I ~/benchmark/include/ -g -O4 -Wall -Wextra -Werror -pedantic --std=c++14 \
     ~/benchmark/build/src/libbenchmark.a -lpthread -lrt -lm -o $filename
